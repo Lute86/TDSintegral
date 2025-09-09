4bc5550d18b67d7db6aadfd1c5e6ad47cfe5cfc4
Sato <116687199+Lute86@users.noreply.github.com>
2025-09-09 21:16:33 +0200

    Delete Backend Ejemplo CJs directory

--- Backend Ejemplo CJs/src/middleware/logger.mdw.js
+++
@@ -1,6 +0,0 @@
-// Middleware simple que muestra por consola el metodo y url de la peticion
-function loggingMdw(req, res, next) {
-  console.log(`Request made to ${req.url} with method: ${req.method}.`);
-  next();
-}
-module.exports = loggingMdw
