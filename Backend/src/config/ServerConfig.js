53ee47fe5de7a575be66ecfc9d7dbeaa02f4d886
Sato <s.lutereau@hotmail.com>
2025-09-12 22:17:22 +0200

    rename httpresponse

--- Backend/src/config/ServerConfig.js
+++ Backend/src/config/ServerConfig.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { EmployeeRoutes } from '../routes/Employee.routes.js';
-import HttpResponse from '../utils/HttpResponse.mdw.js';
+import HttpResponse from '../utils/HttpResponse.utils.js';
 
 export class Server{
     constructor(){
--- Backend/src/controllers/Employee.controller.js
+++ Backend/src/controllers/Employee.controller.js
@@ -1,4 +1,4 @@
-import HttpResponse from "../utils/HttpResponse.mdw.js";
+import HttpResponse from "../utils/HttpResponse.utils.js";
 import { EmployeeService } from "../services/Employee.service.js";
 
 export class EmployeeController{
